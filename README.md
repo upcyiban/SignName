7a665c968187fdc6b34ccde2de026ad36855e51d
skyADMIN <546485299@qq.com>
2016-07-18 13:57:01 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# SignName
-石大易签到 - 新版
+# sign-name-new
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name signNameNewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the signNameNewApp
+ */
+angular.module('signNameNewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
